3c269a1d569f48f438b59c7d24b957d93ed2aeea
juanralvaro <juanralvaro@yahoo.es>
2024-02-28 09:34:15 +0100

    first commit

---
+++ ejercicio1.py
@@ -0,0 +1,23 @@
+edad = 42
+nombre = "Juan"
+peso = 68.5
+
+print(edad)
+print(nombre)
+print(peso)
+
+age, name, weight = (42, "John", 69.2)
+
+print(age, name, weight)
+
+print("Mi nombre es " + nombre + " y mi edad es " + str(edad)) #Conversión de variables
+
+#Siempre antes y después de un operador, tengo que incluir un espacio (el símbolor = es en Python el operador de asignación, el que utilizamos para asignar un valor a una variable)
+
+
+print("Mi nombre es " + nombre + ", mi edad es " + str(edad) + ", y mi peso es " + str(edad)+".")
+
+cadena = "Hola"
+print(cadena[0])
+print(cadena[0] + cadena[1] + cadena[2] + cadena [3])
+print(cadena[3] + cadena[2] + cadena[1] + cadena [0])
---
+++ hola-mundo.py
@@ -0,0 +1,12 @@
+# Esto es un comentario de una sola línea
+
+"""Esto es
+un comentario
+de un bloque: los comentarios son fragmentos de código que son ignorados por el intérprete"""
+
+print("46")
+
+nueva_cadena = "soy una nueva variable"
+
+print(nueva_cadena[0:4])
+print(nueva_cadena[3:9])
